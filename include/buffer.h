696075cb36ccf2c7922737fef5d0764bcd69a2aa
Sergey Litvinov <slitvinov@gmail.com>
2020-03-26 13:25:23 +0100

    minor

--- include/buffer.h
+++ include/buffer.h
@@ -1,13 +1,8 @@
-#include <stdio.h>
-
 #define BUFFER_SIZE 8192
 
 #define CHAR 1
 #define WORD 2
 #define LINE 3
-
-
-
 struct _Buffer {
     size_t i, f;                /* next to read and next to write */
     FILE *fp;                   /* last fiel in use */
@@ -15,8 +10,6 @@ struct _Buffer {
     int eof;                    /* if true EOF reached */
     char buf[BUFFER_SIZE];      /* buffer max. size */
 };
-
-
 long RChar(char *cad, FILE * fp);
 long RWord(char *cad, int, FILE * fpq);
 long RLine(char *cad, int, FILE * fp);
