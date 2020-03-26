f0e123504066bc0606fa0dd69abc3ba3d2e511a4
Sergey Litvinov <slitvinov@gmail.com>
2020-03-26 12:17:43 +0100

    mv copyright to COPYING

--- src/buffer.c
+++ src/buffer.c
@@ -1,28 +1,3 @@
-/*****************************************************************************
- **  This is part of the punto program
- **  Copyleft (C) 1998-2009  Mariano Revenga
- **
- **  This program is free software; you can redistribute it and/or modify
- **  it under the terms of the GNU General Public License (version 2) as
- **  published by the Free Software Foundation.
- **
- **  This program is distributed in the hope that it will be useful,
- **  but WITHOUT ANY WARRANTY; without even the implied warranty of
- **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- **  GNU General Public License for more details.
- **
- **  You should have received a copy of the GNU General Public License
- **  along with this program; if not, write to the Free Software
- **  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- ******************************************************************************/
-
-/*
-  buffer.c
-  file reading using a buffer.
-  version 0.02 Jun 2009
-  Author: mrevenga.
-*/
-
 #include <string.h>
 #include <stdlib.h>
 #include "buffer.h"
